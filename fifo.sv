b5237b722f1c55ca0c8f7534eb3644ca6a55b374
Brendan Hendrata <bhendrata@cmu.edu>
2025-12-29 04:09:13 -0500

    implemented read and write logic

--- fifo.sv
+++ fifo.sv
@@ -1,5 +1,7 @@
-module fifo
-#(parameter int WIDTH)
+module fifo #(
+    parameter int WIDTH = 32,
+    parameter int DEPTH = 16
+)
 (
     input  logic clock,
     input  logic reset,
@@ -12,5 +14,40 @@ module fifo
 
     output logic full, empty
 );
+    // Time scale
+    timeunit 1ns; timeprecision 100ps;
+
+    // Local parameters
+    localparam ADDR_WIDTH = $clog2(DEPTH);
+
+    // Registers
+    logic [WIDTH-1:0] mem [0:DEPTH-1];
+
+    // Pointers
+    logic [$clog2(DEPTH):0] wr_ptr, rd_ptr;
+
+    // Write logic
+    always_ff @(posedge clock, posedge reset) begin
+        if (reset)
+            wr_ptr <= 0;
+        else begin
+            if (wr_en && !full) begin
+                mem[wr_ptr] <= wr_data;
+                wr_ptr <= wr_ptr + 1;
+            end
+        end
+    end
+
+    // Read logic
+    always_ff @(posedge clock, posedge reset) begin
+        if (reset)
+            rd_ptr <= 0;
+        else begin
+            if (rd_en && !empty) begin
+                rd_data <= mem[rd_ptr];
+                rd_ptr <= rd_ptr + 1;
+            end
+        end
+    end
 
 endmodule: fifo
